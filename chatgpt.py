d5838da86d95e5cdd3f8f9e854fe07629171b832
Sathish Kumar P <146006106+SKjackcodehub@users.noreply.github.com>
2023-09-28 20:47:32 +0530

    Add files via upload

---
+++ chatgpt.py
@@ -0,0 +1,20 @@
+import openai
+import gradio
+
+openai.api_key = "sk-D3yWxd8ZRNxmuoez8DFgT3BIbkFJ80XRdtvpM92eHQ5Qk"
+
+messages = [{"role": "system", "content": "You are a financial experts that specializes in real estate investment and negotiation"}]
+
+def CustomChatGPT(user_input):
+    messages.append({"role": "user", "content": user_input})
+    response = openai.ChatCompletion.create(
+        model = "gpt-3.5-turbo",
+        messages = messages
+    )
+    ChatGPT_reply = response["choices"][0]["message"]["content"]
+    messages.append({"role": "assistant", "content": ChatGPT_reply})
+    return ChatGPT_reply
+
+demo = gradio.Interface(fn=CustomChatGPT, inputs = "text", outputs = "text", title = "Your chatgpt")
+
+demo.launch(share=True)
